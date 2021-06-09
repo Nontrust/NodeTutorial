4520b6783b627a739a2fd5427afda8c680591b35
소프트웍스 이충호 <deed1515@softworks.co.kr>
2021-06-09 18:24:44 +0900

    node 1

---
+++ view/main.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,resposne){
+    var url = request.url;
+    if(url == '/'){
+        url = '/index.html';
+    }if(url=='/favicon.ico'){
+        return resposne.writeHead(404);
+    }
+    resposne.writeHead(200);
+    console.log(__dirname+url);
+    resposne.end(fs.readFileSync(__dirname+url));
+
+});
+app.listen(3000);
+
